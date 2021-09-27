47a70285c9148b86018c4625c926e14939040bfa
Aremu Smog <aremuoluwagbamila@gmail.com>
2021-09-27 20:25:45 +0100

    fix: vscode intellisense support for absolute paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Test from '@Components/Test'
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "@Components/Test";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Test/>
+        <Test />
       </header>
     </div>
   );
